323eab5810198326b1c8728a8112f84abf459e71
kpikachu101 <kpeak@winsor.edu>
2020-07-08 15:38:45 -0400

    initial commit

---
+++ 7_8-servo-hw.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+Servo myServo;
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22
+#define POTPIN A0
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  myServo.attach(3);
+  Serial.begin(9600);
+  Serial.println(F("test!"));
+
+  dht.begin();
+}
+
+void loop() {
+ 
+  float t = dht.readTemperature();
+ 
+
+if (isnan(t)){
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+}
+  Serial.print("Your temp is -> ");
+  Serial.println(t);
+
+  int servoPos = map(t,0,150,0,180);
+  myServo.write(servoPos);
+  delay(50);
+}
